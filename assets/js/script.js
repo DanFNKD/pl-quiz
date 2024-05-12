78d5dd1e4ef8f974a47129227e0232cf5e0de0d4
Dan Harris <dwh261@gmail.com>
2024-05-12 13:36:42 +0000

    Added answers for each question in the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,38 @@ document.addEventListener("DOMContentLoaded", function() {
     let score = 0;
     let incorrect = 0;
 
-    
+    let correctAnswerMap = [
+        { question: 'question1', answer: 'Brian Deane' },
+        { question: 'question2', answer: 'Man Utd' },
+        { question: 'question3', answer: 'Man City' },
+        { question: 'question4', answer: 'Alan Shearer' },
+        { question: 'question5', answer: 'Shane Long' },
+        { question: 'question6', answer: 'Arsene Wenger' },
+        { question: 'question7', answer: 'Liverpool & Wimbledon' },
+        { question: 'question8', answer: 'Gareth Barry' },
+        { question: 'question9', answer: 'Brad Friedel' },
+        { question: 'question10', answer: 'Neil Sullivan' },
+        { question: 'question11', answer: 'Mohamed Salah' },
+        { question: 'question12', answer: 'Man City 2017/2018' },
+        { question: 'question13', answer: 'Sam Allardyce' },
+        { question: 'question14', answer: 'Man Utd' },
+        { question: 'question15', answer: 'Craig Bellamy' },
+        { question: 'question16', answer: '49 matches' },
+        { question: 'question17', answer: 'Hugo Lloris' },
+        { question: 'question18', answer: 'Teddy Sheringham' },
+        { question: 'question19', answer: 'Nicolas Anelka' },
+        { question: 'question20', answer: 'Sunderland' },
+        { question: 'question21', answer: 'Chelsea' },
+        { question: 'question22', answer: 'Nemanja Vidic' },
+        { question: 'question23', answer: 'Hull 2016/2017' },
+        { question: 'question24', answer: 'Brad Friedel' },
+        { question: 'question25', answer: 'Mohamed Salah' },
+        { question: 'question26', answer: 'WBA 5-5 MUN' },
+        { question: 'question27', answer: 'Jamie Vardy' },
+        { question: 'question28', answer: 'Spurs v Arsenal' },
+        { question: 'question29', answer: 'Henry & De Bruyne' },
+        { question: 'question30', answer: 'Chris Wood' },
+    ];
+
+
 });
